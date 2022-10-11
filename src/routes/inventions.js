de35420ef3f90db466be2df3dcdcdf1fb1626ead
Steven <stevenagbassah23@gmai.com>
2022-10-11 17:03:05 +0200

    add(inventions.js) : new sort fonctionnality

--- src/routes/inventions.js
+++ src/routes/inventions.js
@@ -15,4 +15,42 @@ router.get('/inventions/:key?', (req, res) => {
 	});
 });
 
+
+router.get('/inventions/sort/desc', (req, res) => {
+	const inventions = Inventions.list()
+	inventions.sort(function compare(a,b) {
+		if(a.creationDate < b.creationDate) return 1;
+		if(a.creationDate > b.creationDate) return -1;
+		return 0;
+
+	}
+	)
+	res.send({ 
+		inventions, 
+		sources: [
+			'https://www.thoughtco.com/20th-century-timeline-1992486',
+			'https://en.wikipedia.org/wiki',
+		] 
+	});
+});
+
+
+router.get('/inventions/sort/asc', (req, res) => {
+	const inventions = Inventions.list()
+	inventions.sort(function compare(a,b) {
+		if(a.creationDate < b.creationDate) return -1;
+		if(a.creationDate > b.creationDate) return 1;
+		return 0;
+
+	}
+	)
+	res.send({ 
+		inventions, 
+		sources: [
+			'https://www.thoughtco.com/20th-century-timeline-1992486',
+			'https://en.wikipedia.org/wiki',
+		] 
+	});
+});
+
 module.exports = router;
