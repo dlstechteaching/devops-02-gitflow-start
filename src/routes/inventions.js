72e74023305c594399c8270c5a0b811d269f5330
Brahim <brahim.abid@student.vinci.be>
2022-11-01 13:21:29 +0100

    feat(inventions): adding sorted route

--- src/routes/inventions.js
+++ src/routes/inventions.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Inventions = require('../models/Invention');
 const keyToUpperCase = require('../services/keyToUpperCase');
+const sortPerCreationDate = require("../services/sortPerCreationDate");
 
 const router = express.Router();
 
@@ -14,5 +15,17 @@ router.get('/inventions/:key?', (req, res) => {
 		] 
 	});
 });
+
+router.get('/inventions/sort/:orderType', (req, res) => {
+	const inventions = sortPerCreationDate(Inventions.list(), req.params.orderType);
+	res.send({ 
+		inventions, 
+		sources: [
+			'https://www.thoughtco.com/20th-century-timeline-1992486',
+			'https://en.wikipedia.org/wiki',
+		] 
+	});
+});
+
 
 module.exports = router;
---
+++ src/services/sortPerCreationDate.js
@@ -0,0 +1,6 @@
+module.exports = function (items, sortType) {
+  const sortedItems = items.sort(
+    (item1, item2) => item1.creationDate - item2.creationDate
+  );
+  return sortType === "asc" ? sortedItems : sortedItems.reverse();
+};
