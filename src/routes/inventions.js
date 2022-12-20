0903a003b76f9fc464151f8ab13dda6401b6ef68
NJKatorz <nathanesakwa@gmail.com>
2022-12-20 23:29:18 +0100

    feat(inventions): adding sorted route

--- src/routes/inventions.js
+++ src/routes/inventions.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const Inventions = require('../models/Invention');
 const keyToUpperCase = require('../services/keyToUpperCase');
+const sortByCreationDate = require('../services/sortByCreationDate');
 
 const router = express.Router();
+
+const sources = [
+	'https://www.thoughtco.com/20th-century-timeline-1992486',
+	'https://en.wikipedia.org/wiki',
+];
+
 
 router.get('/inventions/:key?', (req, res) => {
 	const inventions = keyToUpperCase(Inventions.list(), req.params.key ?? 'author');
 	res.send({ 
 		inventions, 
-		sources: [
-			'https://www.thoughtco.com/20th-century-timeline-1992486',
-			'https://en.wikipedia.org/wiki',
-		] 
+		sources
 	});
 });
+
+router.get('/inventions/sort/:orderType', (req, res) => {
+	const inventionsOrderBy = sortByCreationDate(Inventions.list(), req.params.orderType);
+	res.send({ 
+		inventions : inventionsOrderBy, 
+		sources
+	});
+});
+
+
 
 module.exports = router;
